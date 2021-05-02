4a8e333635c80df58d7fd95e9afb9ecde03494f5
Artem <dr.artem-k@yandex.ru>
2021-05-02 22:04:21 +0400

    add btn in constructor

--- src/components/BurgerConstructor/BurgerConstructor.js
+++ src/components/BurgerConstructor/BurgerConstructor.js
@@ -1,39 +1,49 @@
 import React from "react";
-import { ConstructorElement, DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { ConstructorElement, DragIcon, Button, CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./BurgerConstructor.module.css";
-import cn from 'classnames';
+import cn from "classnames";
 
 export function BurgerConstructor() {
   const img = "https://code.s3.yandex.net/react/code/bun-02.png";
   return (
-    <div className={ cn(styles.container, 'mt-25') }>
-      <div className={"ml-8"}>
+    <div className={cn(styles.container)}>
+      <div className={cn(styles.element, "mb-5")}>
         <ConstructorElement type="top" isLocked={true} text="Краторная булка N-200i (верх)" price={200} thumbnail={img} />
       </div>
-      <ul className= {styles.list}>
-        <li className={styles.item}>
+      <ul className={styles.list}>
+        <li className={cn(styles.item, "mb-5")}>
           <DragIcon type="primary" />
           <ConstructorElement text="Краторная булка N-200i (верх)" price={50} thumbnail={img} />
         </li>
-        <li className={styles.item}>
+        <li className={cn(styles.item, "mb-5")}>
           <DragIcon type="primary" />
           <ConstructorElement text="Краторная булка N-200i (верх)" price={50} thumbnail={img} />
         </li>
-        <li className={styles.item}>
+        <li className={cn(styles.item, "mb-5")}>
           <DragIcon type="primary" />
           <ConstructorElement text="Краторная булка N-200i (верх)" price={50} thumbnail={img} />
         </li>
-        <li className={styles.item}>
+        <li className={cn(styles.item, "mb-5")}>
           <DragIcon type="primary" />
           <ConstructorElement text="Краторная булка N-200i (верх)" price={50} thumbnail={img} />
         </li>
-        <li className={styles.item}>
+        <li className={cn(styles.item, "mb-5")}>
           <DragIcon type="primary" />
           <ConstructorElement text="Краторная булка N-200i (верх)" price={50} thumbnail={img} />
         </li>
       </ul>
-      <div className={"ml-8"}>
+      <div className={cn(styles.element, "mb-10")}>
         <ConstructorElement type="bottom" isLocked={true} text="Краторная булка N-200i (низ)" price={200} thumbnail={img} />
+      </div>
+
+      <div className={styles.total}>
+        <div className={cn(styles.price, 'mr-10')}>
+          <span className="text text_type_digits-default">610</span>
+          <CurrencyIcon type="primary" />
+        </div>
+        <Button type="primary" size="large">
+          Нажми на меня
+        </Button>
       </div>
     </div>
   );
