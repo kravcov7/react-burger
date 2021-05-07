24c773efbf7087b863cad55ac810e2754c4c7cf7
Artem <dr.artem-k@yandex.ru>
2021-05-07 23:41:24 +0400

    styles ingred modal

--- src/components/ingredient-details/ingredient-details.js
+++ src/components/ingredient-details/ingredient-details.js
@@ -3,32 +3,30 @@ import s from './ingredient-details.module.css';
 import cn from 'classnames';
 
 function IngredientsDetails({ name, image, carbohydrates, fat, proteins, calories }) {
-// function IngredientsDetails(props) {
-  // console.log(props);
-  
+
   return (
-    <div className={s.ingredients}>
-      <h1 className={s.title}>Детали ингредиента</h1>
+    <div className={s.ingredient}>
+      <h1 className={cn(s.title, 'text', 'text_type_main-large', 'mb-5')}>Детали ингредиента</h1>
       <div className={s.container}>
-        <img className={s.container__image} src={image} alt='' />
-        <h2 className={s.container__subtitle}>{ name }</h2>
-        <p className={s.container__text}>Превосходные котлеты из марсианской Магнолии для фирменных космических бургеров, набирающих популярность по всей вселенной.</p>
-        <ul className={s.box}>
-          <li className={s.box__item}>
+        <img className={s.container__image} src={image} alt='ингредиент' />
+        <h2 className={cn(s.container__subtitle, 'text', 'text_type_main-medium')}>{ name }</h2>
+        <p className={cn(s.container__subtitle, 'mb-8', 'mt-8', 'text', 'text_type_main-default')}>Превосходные котлеты из марсианской Магнолии для фирменных космических бургеров, набирающих популярность по всей вселенной.</p>
+        <ul className={cn(s.box, 'mt-8')}>
+          <li className={cn(s.box__item, 'text', 'text_type_main-default', 'text_color_inactive')}>
             <p className={s.box__text}>Калории, ккал</p>
-            <p className={cn('text_type_digits-default')}>{ calories }</p>            
+            <p className={cn(s.box__sum, 'text_type_digits-default')}>{ calories }</p>            
           </li>
-          <li className={s.box__item}>
+          <li className={cn(s.box__item, 'text', 'text_type_main-default', 'text_color_inactive')}>
             <p className={s.box__text}>Белки, г</p>
-            <p className={cn('text_type_digits-default')}>{ proteins }</p>            
+            <p className={cn(s.box__sum, 'text_type_digits-default')}>{ proteins }</p>            
           </li>
-          <li className={s.box__item}>
+          <li className={cn(s.box__item, 'text', 'text_type_main-default', 'text_color_inactive')}>
             <p className={s.box__text}>Жиры, г</p>
-            <p className={cn('text_type_digits-default')}>{ fat }</p>            
+            <p className={cn(s.box__sum, 'text_type_digits-default')}>{ fat }</p>            
           </li>
-          <li className={s.box__item}>
+          <li className={cn(s.box__item, 'text', 'text_type_main-default', 'text_color_inactive')}>
             <p className={s.box__text}>Углеводы, г</p>
-            <p className={cn('text_type_digits-default')}>{ carbohydrates }</p>            
+            <p className={cn(s.box__sum, 'text_type_digits-default')}>{ carbohydrates }</p>            
           </li>
         </ul>
 
