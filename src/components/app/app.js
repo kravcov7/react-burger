9d6390e55af4945b575ae5786eef5b684b5ca9e3
Artem <dr.artem-k@yandex.ru>
2021-05-31 18:13:04 +0400

    fix scroll

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { setConstantValue } from 'typescript';
 
 import AppHeader from '../app-header/app-header';
 import { BurgerBlock} from '../burger-block/burger-block';
+import s from './App.module.css'
 
 function App() {  
   return (
-    <>
+    <div className={s.container}>
       <AppHeader />
       <BurgerBlock />           
-    </>
+    </div>
   );
 }
 
