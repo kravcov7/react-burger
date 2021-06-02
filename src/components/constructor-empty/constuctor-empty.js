22f302ad3f7d5a8b11915e3b749e0820a00b9ec1
Artem <dr.artem-k@yandex.ru>
2021-06-02 11:33:50 +0400

    style empty elem

--- src/components/constructor-empty/constuctor-empty.js
+++ src/components/constructor-empty/constuctor-empty.js
@@ -1,10 +1,11 @@
 import React from "react";
 import s from "./constructor-empty.module.css";
+import cn from 'classnames';
 
 function ConstructorEmpty() {
   return (
-    <div className={s.container}>
-      <h2>Соберите бургер</h2>
+    <div className={cn(s.container, 'text', 'text_type_main-large')}>
+      <h2 className={s.title}>Соберите бургер</h2>
       <p>Перетащите ингредиенты из левой части </p>
     </div>
   );
