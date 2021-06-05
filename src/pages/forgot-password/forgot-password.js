aa1cab44fe2715b169fd6f7ce78aa52c59795791
Artem <dr.artem-k@yandex.ru>
2021-06-05 21:50:51 +0400

    кнопка восстановить

--- src/pages/forgot-password/forgot-password.js
+++ src/pages/forgot-password/forgot-password.js
@@ -1,22 +1,32 @@
-import React from "react";
+import {useState} from 'react';
 import { Input, Button } from "@ya.praktikum/react-developer-burger-ui-components";
 import cn from "classnames";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import s from "./forgot-password.module.css";
 
 function ForgotPassword() {
+  const [email, setEmail] = useState('');
+
+  const recoveryClick = (event) => {
+    // event.preventDefault;
+    console.log(email);
+  };
+
   return (
     <section className={cn(s.main, "mt-30")}>
       <form className={s.form}>
         <h1 className="text text_type_main-large mb-6">Восстановление пароля</h1>
-        <Input type={"text"} placeholder={"Укажите e-mail"} size={"default"} />
-        <Button type="primary" size="large" className="mt-6">
+        <Input type={"email"} placeholder={"Укажите e-mail"} value={email} onChange={(event) => setEmail(event.target.value)} />
+        <Button onClick={recoveryClick} type="primary" size="large" className="mt-6">
           Восстановить
         </Button>
       </form>
       <p className="text text_type_main-default text_color_inactive mt-20">
-        Вспомнили пароль?<Link to="/login" type="secondary" className={cn(s.link, 'ml-2')}>Войти</Link>
+        Вспомнили пароль?
+        <Link to="/login" type="secondary" className={cn(s.link, "ml-2")}>
+          Войти
+        </Link>
       </p>
     </section>
   );
--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { EmailInput, Input, Button, PasswordInput } from "@ya.praktikum/react-developer-burger-ui-components";
 import cn from "classnames";
+import {Link} from 'react-router-dom';
 
 import s from "./register.module.css";
 
@@ -17,7 +18,7 @@ function Register() {
         </Button>
       </form>
       <p className="text text_type_main-default text_color_inactive mt-20">
-        Уже зарегистрированы?<Button type="secondary" className={s.link}>Войти</Button>
+        Уже зарегистрированы?<Link to="/login"  type="secondary" className={cn(s.link, 'ml-2')}>Войти</Link>
       </p>
     </section>
   );
