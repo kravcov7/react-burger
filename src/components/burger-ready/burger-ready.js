46470f7c172e8ba80cc9e878777181089449888d
Artem <dr.artem-k@yandex.ru>
2021-06-05 22:38:33 +0400

    бургеры в feed рнедерятся из массива и переход feed/id

--- src/components/burger-ready/burger-ready.js
+++ src/components/burger-ready/burger-ready.js
@@ -1,18 +1,20 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import Burger from "../burger/burger";
 
 import cn from "classnames";
 import s from "./burger-ready.module.css";
+import BurgerConstructorElements from "../burg-constr-elements/burger-constructor-elements";
 
 export function BurgerReady() {
+  const burgers = [ 1, 2, 3, 4, 5]
   return (
-    <section className={cn(s.main, 'mt-10')}>
+    <section className={cn(s.main, "mt-10")}>
       <h1 className="text text_type_main-large mb-5">Лента заказов</h1>
-      <Burger />
-      <Burger />
-      <Burger />
-      <Burger />
-      <Burger />
+      {burgers.map((element, index) => (
+        <Link key={index} className={s.link} to={`/feed/${index}`}>
+          <Burger />
+        </Link>
+      ))}
     </section>
   );
 }
