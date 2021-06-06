27336e5543820a38732fdb276530c6d3ede31b24
Artem <dr.artem-k@yandex.ru>
2021-06-06 14:51:49 +0400

    добавление запроса в register, login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,26 +1,73 @@
-import React from "react";
-import { EmailInput, Input, Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import React, { useState } from "react";
+import { PasswordInput, Input, Button } from "@ya.praktikum/react-developer-burger-ui-components";
 import cn from "classnames";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import s from "./login.module.css";
 
 function Login() {
+  const [state, setState] = useState({
+    name: "",
+    password: "",
+  });
+
+  const login = ({ name, password }) => {
+    fetch("https://norma.nomoreparties.space/api/auth", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, password }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`ошибка: ` + res.status);
+        } else {
+          return res.json();
+        }
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
+  const handleInputChange = (event) => {
+    const target = event.target;
+    // Определяем, откуда пришло событие: из чекбокса или текстового поля ввода
+    const value = target.value;
+    const name = target.name;
+
+    // Применяем вычисляемые имена свойств
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(state);
+    login(state);
+  };
+
   return (
     <section className={cn(s.main, "mt-30")}>
-      <form className={s.form}>
+      <form onSubmit={submit} className={s.form}>
         <h1 className="text text_type_main-large mb-6">Вход</h1>
-        <EmailInput name={"email"} className="mb-5" />
-        <Input type={"text"} placeholder={"Пароль"} size={"default"} />
+        <Input type={"email"} placeholder={"E-mail"} onChange={handleInputChange} value={state.name} name={"name"} error={false} errorText={"Ошибка"} size={"default"} />
+        <PasswordInput onChange={handleInputChange} value={state.password} name={"password"} />
         <Button type="primary" size="large" className="mt-6">
           Войти
         </Button>
       </form>
       <p className="text text_type_main-default text_color_inactive mt-20">
-        Вы - новый пользователь?<Link to="/register" type="secondary" className={cn(s.link, 'ml-2')}>Зарегестрироваться</Link>{" "}
+        Вы - новый пользователь?
+        <Link to="/register" type="secondary" className={cn(s.link, "ml-2")}>
+          Зарегестрироваться
+        </Link>{" "}
       </p>
       <p className="text text_type_main-default text_color_inactive mt-4">
-        Забыли пароль?<Link  to="/forgot-password" type="secondary" className={cn(s.link, 'ml-2')}>Восстановить пароль</Link>
+        Забыли пароль?
+        <Link to="/forgot-password" type="secondary" className={cn(s.link, "ml-2")}>
+          Восстановить пароль
+        </Link>
       </p>
     </section>
   );
--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { EmailInput, Input, Button, PasswordInput } from "@ya.praktikum/react-developer-burger-ui-components";
 import cn from "classnames";
 import {Link} from 'react-router-dom';
@@ -6,13 +6,54 @@ import {Link} from 'react-router-dom';
 import s from "./register.module.css";
 
 function Register() {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const register = ({ name, password, email }) => {
+    fetch("https://norma.nomoreparties.space/api/auth/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, password, email }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`ошибка: ` + res.status);
+        } else {
+          return res.json();
+        }
+      })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
+  const handleInputChange = (event) => {
+    const target = event.target;
+    // Определяем, откуда пришло событие: из чекбокса или текстового поля ввода
+    const value = target.value;
+    const name = target.name;
+
+    // Применяем вычисляемые имена свойств
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(state);
+    register(state);
+  };
   return (
     <section className={cn(s.main, "mt-30")}>
-      <form className={s.form}>
+      <form onSubmit={submit} className={s.form}>
         <h1 className="text text_type_main-large mb-6">Регистрация</h1>
-        <Input type={"text"} placeholder={"Имя"} size={"default"} />
-        <EmailInput name={"E-ddddil"} className="mb-5" />
-        <PasswordInput name={"password"} />
+        <Input type={"text"} placeholder={"Имя"} onChange={handleInputChange} value={state.name} name={"name"} error={false} errorText={"Ошибка"} size={"default"} />
+        <Input type={"email"} placeholder={"E-mail"} onChange={handleInputChange} value={state.email} name={"email"} error={false} errorText={"Ошибка"} size={"default"} />
+        <PasswordInput onChange={handleInputChange} value={state.password} name={"password"} />
         <Button type="primary" size="large" className="mt-6">
           Зарегистрироваться
         </Button>
