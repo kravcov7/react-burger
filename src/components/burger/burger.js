9296ef33dae51d381cc7615843f9898f360698db
Artem <dr.artem-k@yandex.ru>
2021-06-07 15:57:16 +0400

    статус заказа

--- src/components/burger/burger.js
+++ src/components/burger/burger.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
-// import cn from "classnames";
+import cn from "classnames";
 import s from "./burger.module.css";
 import BurgerImage from "../burger-image/burger-image";
+import PropTypes from "prop-types";
 
-export function Burger({name}) {
+export function Burger({ name, status }) {
+  let text = "";
+  
+  if (status === "completed") text = "Выполнен";
+  // if (status === "completed") return { text: 'Выполнен', color: 'green'};
+  if (status === "canceled") text = "Отменен";
+ 
   return (
     <section className={s.main}>
       <div className="p-6">
@@ -13,10 +20,12 @@ export function Burger({name}) {
           <span className="text text_type_digits-default">#034535</span>
           <div className="text text_type_main-default text_color_inactive">Сегодня, 16:20 i-GMT+3</div>
         </div>
-        <h2 className="text text_type_main-medium mt-6 mb-6">{ name}</h2>
+        <h2 className="text text_type_main-medium mt-6 mb-2">{name}</h2>
+        {/* <h2 className={cn(s[`status_color_${color}`], "text text_type_main-default mb-26")}>{ st.text }</h2> */}
+        <h2 className={cn(s.status_color_green, "text text_type_main-default mb-26")}>{text}</h2>
         <div className={s.footer}>
           <ul className={s.container}>
-            <li className={s.item}  style={{ zIndex: 4 }}>
+            <li className={s.item} style={{ zIndex: 4 }}>
               <BurgerImage />
             </li>
             <li className={s.item} style={{ zIndex: 3 }}>
@@ -41,5 +50,9 @@ export function Burger({name}) {
     </section>
   );
 }
+
+Burger.propTypes = {
+  name: PropTypes.string.isRequired,
+};
 
 export default Burger;
--- src/components/profile-orders/profile-orders.js
+++ src/components/profile-orders/profile-orders.js
@@ -1,23 +1,18 @@
 import { Link } from "react-router-dom";
-// import cn from "classnames";
 import Burger from "../burger/burger";
-import { data } from '../../utils/data'
+import { data } from "../../utils/data";
 
 import s from "./profile-orders.module.css";
 
 export function ProfileOrders() {
-  console.log(data);
   return (
     <div className={s.burgers}>
-    {data.map((element, index) => {
-      console.log(element.name);
-    return (
-      <Link key={index} className={s.burgerlink} to={`/profile/orders/${index}`}>
-        <Burger name={element.name} />
-      </Link>
-    )}
-    )}
-  </div>
+      {data.map((element, index) => (
+        <Link key={index} className={s.burgerlink} to={`/profile/orders/${index}`}>
+          <Burger name={element.name} status={"completed"} />
+        </Link>
+      ))}
+    </div>
   );
 }
 
