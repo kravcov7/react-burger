554d2ed279305a04b6f67cea011bdb067071c229
Artem <dr.artem-k@yandex.ru>
2021-06-26 18:04:42 +0400

    socket begin

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { routerMiddleware, ConnectedRouter } from "connected-react-router";
 import { rootReducer, history } from './services/reducers';
+import socketMiddleware from './services/middlewares/socketMiddleware';
 
 import App from "./components/app/app";
 import reportWebVitals from "./reportWebVitals";
@@ -12,10 +13,30 @@ import { compose, createStore, applyMiddleware } from "redux";
 
 import thunk from "redux-thunk";
 
+import {
+  WS_CONNECTION_CLOSED,
+  WS_CONNECTION_ERROR,
+  WS_CONNECTION_START,
+  WS_CONNECTION_SUCCESS,
+  WS_GET_MESSAGE,
+  WS_SEND_MESSAGE
+} from './services/actions/socketsActions';
+
+const wsActions = {
+  wsInit: WS_CONNECTION_START,
+  wsSendMessage: WS_SEND_MESSAGE,
+  onOpen: WS_CONNECTION_SUCCESS,
+  onClose: WS_CONNECTION_CLOSED,
+  onError: WS_CONNECTION_ERROR,
+  onMessage: WS_GET_MESSAGE
+};
+
 const composeEnhancers = typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 
+const wsUrl = 'wss://norma.nomoreparties.space/orders/all';
+
 const enhancer = composeEnhancers(
-  applyMiddleware(thunk, routerMiddleware(history))
+  applyMiddleware(thunk, routerMiddleware(history), socketMiddleware(wsUrl, wsActions))
 );
 
 const store = createStore(rootReducer, enhancer);
